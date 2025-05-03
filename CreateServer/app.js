1298ac4a9967f213e5fc510ffb43902fdf4a0db5
Shyam Sundar <ShaShyUnixone@gmail.com>
2025-05-03 20:14:36 +0530

    Streams & Buffers

--- CreateServer/app.js
+++ CreateServer/app.js
@@ -1,6 +1,44 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res)=>{
-    console.log(req);
+
+    const url = req.url;
+    const method = req.method;
+    if (url === '/') {
+        res.setHeader('Content-type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title>Enter Form Details</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><input type="submit" value="Send"></form></body>');
+        res.write('</html>');
+        return res.end();    
+    }
+
+    if (url === '/message' && method == "POST") {
+        
+        const body = [];
+        req.on('data',(chunk)=>{            
+            body.push(chunk);
+            console.log(chunk);
+        })
+
+        req.on('end',()=>{
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=');
+            fs.writeFileSync("hello.txt", message[1])
+        })
+
+        fs.writeFileSync('hello.txt', 'Dummy');
+        res.setHeader('Location', '/');
+        res.statusCode = 302;
+        return res.end();
+    }
+
+    res.setHeader('Content-type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>Node Server</title></head>');
+    res.write('<body><h1>Response form Node Server!</h1></body>');
+    res.write('</html>');
+    res.end();
 });
 server.listen(5000);
