63e37eb538c0b8a2aa61f852c9a2d95cfb22ec17
Jayagopal-web <jayagopal369@gmail.com>
2025-05-04 11:24:00 +0530

    commit

--- CreateServer/app.js
+++ CreateServer/app.js
@@ -37,7 +37,7 @@ const server = http.createServer((req, res)=>{
     res.setHeader('Content-type', 'text/html');
     res.write('<html>');
     res.write('<head><title>Node Server</title></head>');
-    res.write('<body><h1>Response form Node Server!</h1></body>');
+    res.write('<body><h1>Response form Node Server</h1></body>');
     res.write('</html>');
     res.end();
 });
